c40a361f67f23e751a6762a8ab7de296bc7a5970
newby101 <ishimwekevin45@gmail.com>
2023-12-19 16:39:26 -0500

    minor fixes on ui new listener on socket io

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -8,11 +8,12 @@ import { NavLink } from "react-router-dom";
 function Chat({ socket }) {
   const [message, setmessage] = useState("");
   const [users, setusers] = useState([]);
+  const [nickname, setnickname] = useState("randomnoob1");
+  const [nickname2, setnickname2] = useState(false);
   const [my_id, setmy_id] = useState(null);
   const [typing, settyping] = useState(false);
   const getMessages = (socket) => {
     socket.on("chat", (data) => {
-      setmy_id(socket.id);
       console.log("connected to live chat", data);
       setusers((prev) => [...prev, data]);
     });
@@ -29,34 +30,59 @@ function Chat({ socket }) {
       console.log(e.message);
     }
   }, []);
+  useEffect(() => {
+    const nick_init = JSON.parse(localStorage.getItem("nickname"));
+    if (localStorage.getItem("nickname")) {
+      setnickname(nick_init);
+    }
+    socket.on("userconnected", (data) => {
+      console.log(data, localStorage.getItem("chatId"));
+      if (data.id && data.id !== localStorage.getItem("chatId")) {
+        console.log(data);
+        setnickname2(data.name);
+      }
+    });
+    socket.emit("userconnected", {
+      name: nick_init ? nick_init : nickname,
+      id: socket.id,
+    });
+  }, [socket.id]);
   return (
     <div>
-      <div className="fixed  top-0 bg-white h-[4em] flex items-center md:px-5 py-2 justify-between w-full">
-        <div className="flex items-center flex-wrap md:flex-nowrap py-2">
-          <img
-            src="https://cdn.britannica.com/79/4479-050-6EF87027/flag-Stars-and-Stripes-May-1-1795.jpg"
-            alt=""
-            className="h-5 md:mx-1"
-          />
-          <h1 className="font-['Oswald']">user@1291</h1>
-          <NavLink to={"/"} className={"ml-2 font-bold"}>
-            \ home
-          </NavLink>
+      <div className="fixed  top-0 bg-white w-full ">
+        <div className="h-[4em] flex items-center md:px-5 py-2 justify-between w-full">
+          <div className="flex items-center flex-wrap md:flex-nowrap py-2">
+            <img
+              src="https://cdn.britannica.com/79/4479-050-6EF87027/flag-Stars-and-Stripes-May-1-1795.jpg"
+              alt=""
+              className="h-5 md:mx-1"
+            />
+            <h1 className="font-['Oswald']">@{nickname}</h1>
+            <NavLink to={"/"} className={"ml-2 font-bold"}>
+              home
+            </NavLink>
+          </div>
+          <div className=" flex">
+            <Buttongrad
+              text=" random video"
+              icon={<FaVideo className="text-3xl mr-2" />}
+              bg="black"
+            />
+            <button className="flex items-center font-bold rounded-full px-4 py-2 bg-red-500 text-white hover:scale-105 transition-all ">
+              <IoRefresh className="text-xl font-bold" />
+              new user
+            </button>
+          </div>
         </div>
-        <div className=" flex">
-          <Buttongrad
-            text=" random video"
-            icon={<FaVideo className="text-3xl mr-2" />}
-            bg="black"
-          />
-          <button className="flex items-center font-bold rounded-full px-4 py-2 bg-red-500 text-white hover:scale-105 transition-all ">
-            <IoRefresh className="text-xl font-bold" />
-            new user
-          </button>
+        <div
+          className={` w-full flex`}
+          style={{ background: nickname2 == false ? "#f0adad" : "#adf0ad" }}
+        >
+          connected to #{nickname2 == false ? <p>none</p> : nickname}
         </div>
       </div>
 
-      <div className="mt-5 pt-[7vh]">
+      <div className="mt-12 pt-[7vh]">
         {users.map((item) => {
           return (
             <div
@@ -108,6 +134,7 @@ function Chat({ socket }) {
           />
         </div>
         <input
+          autoFocus
           type="text"
           className="w-full py-2  px-3 border-2 rounded-full  border-black focus:outline-none font-['Oswald']"
           placeholder="Enter message"
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import CamStream from "../components/camera";
+import { RxCross2 } from "react-icons/rx";
 
 function Landing() {
   const [interests, setinterests] = useState([{ int: "random", bg: "red" }]);
-
+  const [init, setinit] = useState(true);
   const setInterset = (e) => {
     const set = Math.floor(Math.random() * 16777215).toString(16);
     const bg = "#" + "0".repeat(6 - set.length) + set;
     const tag = e.target.value;
     setinterests((prev) => [...prev, { int: tag, bg: bg }]);
     e.target.value = "";
+    setinit(false);
   };
-  console.log(interests);
+  const removeInterest = (id) => {
+    console.log("removeInterst", id);
+    let placeholder = interests;
+    for (let i = 0; i < interests.length; i++) {
+      console.log(interests[i]);
+      if (interests[i].int == id) {
+        console.log(interests[i], "this");
+        setinterests((prev) => [
+          ...placeholder.slice(0, i),
+          ...placeholder.slice(i + 1),
+        ]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (init && localStorage.getItem("interests")) {
+      setinterests(JSON.parse(localStorage.getItem("interests")));
+    }
+  }, []);
+  useEffect(() => {
+    if (!init) {
+      localStorage.setItem("interests", JSON.stringify(interests));
+    }
+  }, [interests]);
+
+  const styleNickname = (e) => {
+    e.target.classList.add("bg-[black]");
+    e.target.classList.add("text-white");
+    e.target.classList.add("font-bold");
+    e.target.blur();
+  };
   return (
     <div>
       <Navbar />
@@ -26,7 +59,25 @@ function Landing() {
             type="text"
             autoFocus
             placeholder="Enter nick name"
-            className="mt-3 w-full border-b-2 border-black py-3 bg-inherit placeholder:text-[#854a4a] focus:outline-none"
+            className="px-2 mt-3 w-full border-b-2 border-black py-3 bg-inherit placeholder:text-[#854a4a] focus:outline-none"
+            onChange={(e) => {
+              localStorage.setItem("nickname", JSON.stringify(e.target.value));
+            }}
+            onFocus={(e) => {
+              console.log("onFocus", e.target);
+              e.target.classList.remove("bg-[black]");
+              e.target.classList.remove("text-white");
+            }}
+            onBlur={(e) => {
+              if (e.target.value.length > 1) {
+                styleNickname(e);
+              }
+            }}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                styleNickname(e);
+              }
+            }}
           />
           <input
             type="text"
@@ -44,15 +95,23 @@ function Landing() {
             }}
             className="mt-1 w-[20%] border-b-2 border-black py-3 bg-inherit placeholder:text-[#854a4a] focus:outline-none"
           />
-          <div className="flex">
+          <div className="flex flex-wrap">
             {interests.map(({ int, bg }) => {
               return (
                 <p
                   key={bg}
                   style={{ background: bg }}
-                  className={` px-4 w-fit  mt-2 py-1 ext-black mx-1`}
+                  className={` px-4 w-fit  mt-2 py-2 ext-black mx-1 flex relative`}
                 >
                   {int}
+                  <button
+                    id={int}
+                    onClick={(e) => {
+                      removeInterest(e.target.parentNode.id);
+                    }}
+                  >
+                    <RxCross2 className="text-xl py-1 px-1 font-extralight absolute right-0 top-0 text-white hover:scale-105 transition-all" />
+                  </button>
                 </p>
               );
             })}
